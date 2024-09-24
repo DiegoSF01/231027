19d7dd47fe4d660381b8cf42ab377fc03be1568e
Usuario <Usuario@CPD03-M17>
2024-09-24 12:23:53 -0300

    Adicionar arquivos de projeto.

---
+++ 231027/Banco.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Crmf;
+
+namespace _231027
+{
+    internal class Banco
+    {
+        // Criando as variáveis publicas para conexão  e consultaserão usadas em todoo projeto
+        // Connection responsável pela conexão com o MySQL
+        public static MySqlConnection Conexao;
+        // Command responsável pelas instruções SQL a serem executadas
+        public static MySqlCommand Comando;
+        // Adapter responsável por inserir dados em um tadaTable
+        public static MySqlDataAdapter Adaptador;
+        // DataTable responsável por ligar o banco em controles com a propriedade DataSource
+        public static DataTable datTabela;
+
+        public static void AbrirConexao()
+        {
+            try
+            {
+                //Estabelece os parâmetros para a conexão com o banco
+                Conexao = new MySqlConnection("server=localhost;port=3307;uid=root;pwd=etecjau");
+
+                // Abre a conexão com o banco de dados
+                Conexao.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                // Fecha a Conexao com o banco de dados
+                Conexao.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                AbrirConexao();
+
+                Comando = new MySqlCommand("CREATE DATABASE IF NOT EXISTS vendas; USE vendas", Conexao);
+                // Executa a query no MySQL (raio do workbench)
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS Cidades " +
+                                           "(id integer auto_increment primary key, " +
+                                           "nome char(40), " +
+                                           "uf char(02))", Conexao);
+                Comando.ExecuteNonQuery();
+
+                // Chama a função para fechar a conexao com o banco
+                FecharConexao();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ 231027/Views/FrmCidades.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace _231027.Views
+{
+    partial class FrmCidades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmCidades));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.incluir = new System.Windows.Forms.Button();
+            this.alterar = new System.Windows.Forms.Button();
+            this.cancelar = new System.Windows.Forms.Button();
+            this.excluir = new System.Windows.Forms.Button();
+            this.fechar = new System.Windows.Forms.Button();
+            this.pesquisar = new System.Windows.Forms.Button();
+            this.dgvcidades = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcidades)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cidade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(665, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(742, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Digite o nome da Cidade para Pesquisar:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 202);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(586, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(634, 88);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(113, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(27, 88);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(586, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(631, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "UF:";
+            // 
+            // incluir
+            // 
+            this.incluir.Location = new System.Drawing.Point(27, 138);
+            this.incluir.Name = "incluir";
+            this.incluir.Size = new System.Drawing.Size(75, 23);
+            this.incluir.TabIndex = 9;
+            this.incluir.Text = "Incluir";
+            this.incluir.UseVisualStyleBackColor = true;
+            // 
+            // alterar
+            // 
+            this.alterar.Location = new System.Drawing.Point(127, 138);
+            this.alterar.Name = "alterar";
+            this.alterar.Size = new System.Drawing.Size(75, 23);
+            this.alterar.TabIndex = 11;
+            this.alterar.Text = "Alterar";
+            this.alterar.UseVisualStyleBackColor = true;
+            // 
+            // cancelar
+            // 
+            this.cancelar.Location = new System.Drawing.Point(224, 138);
+            this.cancelar.Name = "cancelar";
+            this.cancelar.Size = new System.Drawing.Size(75, 23);
+            this.cancelar.TabIndex = 13;
+            this.cancelar.Text = "Cancelar";
+            this.cancelar.UseVisualStyleBackColor = true;
+            // 
+            // excluir
+            // 
+            this.excluir.Location = new System.Drawing.Point(319, 138);
+            this.excluir.Name = "excluir";
+            this.excluir.Size = new System.Drawing.Size(75, 23);
+            this.excluir.TabIndex = 15;
+            this.excluir.Text = "Excluir";
+            this.excluir.UseVisualStyleBackColor = true;
+            // 
+            // fechar
+            // 
+            this.fechar.Location = new System.Drawing.Point(415, 138);
+            this.fechar.Name = "fechar";
+            this.fechar.Size = new System.Drawing.Size(75, 23);
+            this.fechar.TabIndex = 17;
+            this.fechar.Text = "Fechar";
+            this.fechar.UseVisualStyleBackColor = true;
+            // 
+            // pesquisar
+            // 
+            this.pesquisar.Location = new System.Drawing.Point(653, 187);
+            this.pesquisar.Name = "pesquisar";
+            this.pesquisar.Size = new System.Drawing.Size(61, 49);
+            this.pesquisar.TabIndex = 19;
+            this.pesquisar.Text = "Pesquisar";
+            this.pesquisar.UseVisualStyleBackColor = true;
+            // 
+            // dgvcidades
+            // 
+            this.dgvcidades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvcidades.Location = new System.Drawing.Point(27, 249);
+            this.dgvcidades.Name = "dgvcidades";
+            this.dgvcidades.Size = new System.Drawing.Size(586, 189);
+            this.dgvcidades.TabIndex = 20;
+            // 
+            // FrmCidades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvcidades);
+            this.Controls.Add(this.pesquisar);
+            this.Controls.Add(this.fechar);
+            this.Controls.Add(this.excluir);
+            this.Controls.Add(this.cancelar);
+            this.Controls.Add(this.alterar);
+            this.Controls.Add(this.incluir);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCidades";
+            this.Text = "FrmCidades";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcidades)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button incluir;
+        private System.Windows.Forms.Button alterar;
+        private System.Windows.Forms.Button cancelar;
+        private System.Windows.Forms.Button excluir;
+        private System.Windows.Forms.Button fechar;
+        private System.Windows.Forms.Button pesquisar;
+        private System.Windows.Forms.DataGridView dgvcidades;
+    }
+}
